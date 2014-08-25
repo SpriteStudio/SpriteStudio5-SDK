373c5208ac2777a899ae35b71d13b1bad372c5a9
DKAlpha <dkredint@gmail.com>
2014-08-25 12:00:41 +0900

    コードの整理

--- Players/Cocos2d-x/SS5Player.cpp
+++ Players/Cocos2d-x/SS5Player.cpp
@@ -1895,9 +1895,7 @@ void CustomSprite::draw(cocos2d::Renderer *renderer, const cocos2d::Mat4 &transf
     diff = offsetof( ccV3F_C4B_T2F, colors);
     glVertexAttribPointer(kCCVertexAttrib_Color, 4, GL_UNSIGNED_BYTE, GL_TRUE, kQuadSize, (void*)(offset + diff));
 
-	//コマンドにしてみる。
     glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
-//	cocos2d::Sprite::draw(renderer, transform, flags);
 
     CHECK_GL_ERROR_DEBUG();
 
