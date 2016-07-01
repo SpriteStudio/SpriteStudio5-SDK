9d59d0f35def9857d4071e5956f07330561e4e08
Toshiyuki Kurooka <kurooka@webtech.co.jp>
2016-07-01 14:32:46 +0900

    no message

--- Common/Animator/xorshift32.h
+++ Common/Animator/xorshift32.h
@@ -2,14 +2,11 @@
 #ifndef __XORSHIFT32__
 #define __XORSHIFT32__
 
-#if _WIN32
+#ifdef WIN32
 #ifndef uint32_t	
 	typedef unsigned long uint32_t;
 #endif
-#else
-	uint32_t	y;
 #endif
-
 
 class xorshift32
 {
